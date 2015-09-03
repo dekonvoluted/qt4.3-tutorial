5a678f9f2e842f09bc72f1c9556b2aeb6b373ddf
Karthik Periagaram <dekonvoluted@gmail.com>
2015-09-02 19:11:53 -0700

    Tutorial 5 - Building Blocks

--- mywidget.cpp
+++ mywidget.cpp
@@ -1,17 +1,31 @@
 #include <QApplication>
-#include <QPushButton>
 #include <QFont>
+#include <QLCDNumber>
+#include <QPushButton>
+#include <QSlider>
+#include <QVBoxLayout>
 
 #include "mywidget.h"
 
 MyWidget::MyWidget( QWidget* parent ) : QWidget( parent )
 {
-    setFixedSize( 200, 120 );
-
     QPushButton* quit = new QPushButton( "Quit", this );
-    quit->setGeometry( 62, 40, 75, 30 );
     quit->setFont( QFont( "Times", 18, QFont::Bold ) );
 
+    QLCDNumber* lcd = new QLCDNumber( 2 );
+    lcd->setSegmentStyle( QLCDNumber::Filled );
+
+    QSlider* slider = new QSlider( Qt::Horizontal );
+    slider->setRange( 0, 99 );
+    slider->setValue( 0 );
+
     connect( quit, SIGNAL( clicked() ), qApp, SLOT( quit() ) );
+    connect( slider, SIGNAL( valueChanged( int ) ), lcd, SLOT( display( int ) ) );
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget( quit );
+    layout->addWidget( lcd );
+    layout->addWidget( slider );
+    setLayout( layout );
 }
 
